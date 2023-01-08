494dfeb0659c59c00c30f515715c63e49b9dd3c3
firsticz <first@thai.run>
2023-01-09 00:03:07 +0700

    nav

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ export default function Home() {
             <a className={styles.logo}>
               <img src="https://uploads-ssl.webflow.com/62b081964bde0eb62d67b133/63285aae70bf8d04cab05d83_osiris-logo.svg" loading="lazy" alt="" className={styles.logo_img} />
             </a>
+            <nav>
+              
+            </nav>
           </div>
         </div>
       </main>
