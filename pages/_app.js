4fd5f5cf427722c4d373347ea9c34c383ff8788d
firsticz <first@thai.run>
2023-01-13 23:03:05 +0700

    add firebase

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDrWS0I35EIeuX0-KnsK8AO8I6HmNZlMdg",
-  authDomain: "qrra-7cd65.firebaseapp.com",
-  projectId: "qrra-7cd65",
-  storageBucket: "qrra-7cd65.appspot.com",
-  messagingSenderId: "980829579432",
-  appId: "1:980829579432:web:1e88ed512f5aeae37fba9d",
-  measurementId: "G-43FQ8MMQ8E"
-};
 
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
